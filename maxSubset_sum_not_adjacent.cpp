da5e3b92e710bae393da3e519aeb3e7438287175
Abhi-1725 <33417745+Abhi-1725@users.noreply.github.com>
2020-08-30 17:20:35 +0300

    Create maxSubset_sum_not_adjacent.cpp

---
+++ maxSubset_sum_not_adjacent.cpp
@@ -0,0 +1,28 @@
+/* WAF that takes in an array of positive integers and returns the 
+   maximum sum of non-adjacent elements in the array.
+	 
+	 If a sum can't be generated , the function should return 0.
+*/
+
+#include <vector>
+
+using namespace std;
+
+// O(n) time | O(n) space
+int maxSubsetSumNoAdjacent(vector<int> array) {
+  if(array.size() == 0)
+	{
+		return 0;
+	}
+	else if(array.size() == 1)
+	{
+		return array[0];
+	}
+	vector<int> maxSums = array;
+	maxSums[1] = max(array[0], array[1]);
+	for(int i = 2; i < array.size(); i++)
+	{
+		maxSums[i] = max(maxSums[i - 1], maxSums[i - 2] + array[i]);
+	}
+  return maxSums[array.size() - 1];
+}
